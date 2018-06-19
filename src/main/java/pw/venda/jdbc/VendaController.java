6c130aae9c7a5404e3dad367754ad8ff0c693ba8
Walison Moreira <walison.moreira@gmail.com>
2018-06-19 20:59:57 -0300

    Atualização do controller.

--- src/main/java/pw/venda/jdbc/VendaController.java
+++ src/main/java/pw/venda/jdbc/VendaController.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
